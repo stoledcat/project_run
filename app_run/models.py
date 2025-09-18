8b27df05d0fe3b2adfb461923dcf843560a065ba
stoledcat <jetnavo@yandex.ru>
2025-09-18 20:24:35 +0300

    FIX

--- app_run/models.py
+++ app_run/models.py
@@ -1,12 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=50, blank=False)
 
 
 class Run(models.Model):
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField(blank=True)
+    comment = models.TextField()
--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Run
 
-
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
