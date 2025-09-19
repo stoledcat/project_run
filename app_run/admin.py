e76f33761930418aef12154eb82c46edac8db73a
stoledcat <jetnavo@yandex.ru>
2025-09-19 12:49:51 +0300

    сортировка импортов с помощью isort

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from app_run.models import Run
 
 # Register your models here.
--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import viewsets
-from django.conf import settings
+
 from .models import Run
 from .serializers import RunSerializer
 
