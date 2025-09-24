3b6d9ea17b5d8418cdfb196ff7432f99e438c8c2
stoledcat <jetnavo@yandex.ru>
2025-09-24 13:50:42 +0300

    Добавлен поиск по имени и фамилии

--- app_run/views.py
+++ app_run/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 
 from .models import Run
@@ -21,13 +22,15 @@ def company_details(request):
 
 
 class RunViewSet(viewsets.ModelViewSet):
-    queryset = Run.objects.select_related('athlete').all()
+    queryset = Run.objects.select_related("athlete").all()
     serializer_class = RunSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ["first_name", 'last_name']
 
     def get_queryset(self):
         qs = self.queryset
